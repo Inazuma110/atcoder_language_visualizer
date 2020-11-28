10a7ec7f1bd67130537c4b540409e7b33fbbb607
Inazuma110 <c011703534@edu.teu.ac.jp>
2020-11-29 06:21:07 +0900

    test

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: a
-  }
+    message: "hello"
+  },
+  mounted() {
+    axios.get("./json_data/dp_a_all.json")
+      .then(response => {
+        this.message = response.data
+        console.log(response.data)
+      })
+  },
 })
-
 
