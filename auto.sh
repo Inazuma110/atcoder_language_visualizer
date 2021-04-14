d26c5bfe6f0d8d69978de95cce540494858fb04f
Inazuma110 <c011703534@edu.teu.ac.jp>
2021-04-14 15:32:19 +0900

    fix auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd `dirname $0`
-pipenv run python3 ./update_data.py
+$(which pipenv) run python3 ./update_data.py
 git add ./json_data
 git commit -m "update data"
 git push origin main
